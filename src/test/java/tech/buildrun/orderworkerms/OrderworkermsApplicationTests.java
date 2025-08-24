b5eeb787067d496f760a400baca2d2c3523f96f3
wallacevilela7 <wallace.vilela9@gmail.com>
2025-08-24 13:22:39 -0300

    Create springboot app with maven

---
+++ src/test/java/tech/buildrun/orderworkerms/OrderworkermsApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.buildrun.orderworkerms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderworkermsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
